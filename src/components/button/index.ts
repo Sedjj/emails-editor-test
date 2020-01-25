8571ec12a4632aef549def921a0104295a141a74
Sedjj <omeldarl@rambler.ru>
2020-01-25 19:15:15 +0500

    предварительная структура компонентов

---
+++ src/components/button/index.ts
@@ -0,0 +1,27 @@
+import './index.less';
+
+export class Button {
+	private readonly tagName: string;
+	private count: number;
+
+	constructor() {
+		this.count = 0;
+		this.tagName = 'btn';
+		//initializations
+	}
+
+	public addEvent(btn: HTMLElement): void {
+		btn.addEventListener('click', this.onClick, false);
+	}
+
+	public removeEvent(btn: HTMLElement): void {
+		btn.removeEventListener('click', this.onClick, false);
+	}
+
+	private onClick = (): void => {
+		// eslint-disable-next-line no-console
+		console.log(this.count++);
+		// eslint-disable-next-line no-console
+		console.log(this.tagName);
+	};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,65 @@
-console.log('rtetert2423'); // eslint-disable-line no-console
+import './index.less';
+
+interface IEmailsEditor {
+	container: Element | null;
+	name: string;
+}
+
+interface ISubscriber {
+	name: string;
+	emails: string[];
+}
+
+interface IEmit {
+	(item: ISubscriber): void;
+}
+
+export class EmailsEditor {
+	private container: Element | null = null;
+	private emit: IEmit | null = null;
+	private emails: string[] = [];
+
+	constructor(props: IEmailsEditor) {
+		if (props.container != null) {
+			this.container = props.container;
+			let form = document.createElement('div');
+			form.innerText = 'Hello';
+			this.container.append(form);
+		}
+	}
+
+	public setEmails(emails: string[]): void {
+		this.emails = emails;
+		if (this.emit) {
+			this.emit({
+				name: 'setEmails',
+				emails: this.emails
+			});
+		}
+	}
+
+	public getEmails(): string[] {
+		return this.emails;
+	}
+
+	public subscribe(emit: IEmit): void {
+		this.emit = emit;
+	}
+}
+
+if (process.env.NODE_ENV === 'development') {
+	const emailsEditor = new EmailsEditor({
+		container: document.querySelector('#emails-editor'),
+		...{
+			name: '1'
+		}
+	});
+	emailsEditor.setEmails(['john@miro.com']);
+	emailsEditor.subscribe(({name, emails}: ISubscriber): void => {
+		// eslint-disable-next-line no-console
+		console.log('name: ', name, ' emails: ', emails);
+	});
+	emailsEditor.setEmails(['john@miro.com', 'patric@miro.com']);
+	// eslint-disable-next-line no-console
+	console.log('getEmails: ', emailsEditor.getEmails());
+}
